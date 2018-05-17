d5fa452ccf8a1a23a507558004a7106c819e1ea7
Ivan Krasnobaev <ikrasnobai@yandex.ru>
2018-05-17 19:11:50 +0500

    fixes

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ window.onload = function() {
 	addendA.innerHTML = a;
 	addendB.innerHTML = b;
 	context.drawImage(img, 0, 50);
-	draw(x1, x2, a, left, first);
+	draw(xBase, xFirst, a, left, first);
 };
 
 var addendA = document.getElementById("addendA");
@@ -15,25 +15,39 @@ var left = document.getElementsByClassName("addendum")[0];
 var right = document.getElementsByClassName("addendum")[1];
 var first = document.getElementById("firstIn");
 var second = document.getElementById("secondIn");
-var aArr = [6, 7, 8, 9];
-var cArr = [11, 12, 13, 14];
-var rA = Math.floor(Math.random() * aArr.length);
-var rC = Math.floor(Math.random() * cArr.length);
-var a = aArr[rA];
-var c = cArr[rC];
+var a = randomInt(6, 9);
+var c = randomInt(11, 14);
 var b = c - a;
-var x1 = 35.5;
-var x2 = x1 + a * 39;
-var x3 = x2 + b * 39;
+var addendMax = 9;
+var xBase = 35.5;
+var xFirst = xBase + a * 39;
+var xSecond = xFirst + b * 39;
 
-function draw(x1, x2, a, input, id) {
+function draw(first, second, a, input, id) {
+	var yBase = 70;
+	var yMod = yBase - a * 10;
+	var yModLeft = yBase - 8;
+	var yModRight = yBase - 9;
+	var xModLeft = second - 5;
+	var xModRight = second + 4;
 	context.strokeStyle = "purple";
-	context.moveTo(x1, 70);
-	context.bezierCurveTo(x1, -20, x2, -20, x2, 70);
+	context.moveTo(first, yBase);
+	context.bezierCurveTo(first, yMod, second, yMod, second, yBase);
+	context.moveTo(second, yBase);
+	context.lineTo(xModLeft, yModLeft);
+	context.moveTo(second, yBase);
+	context.lineTo(xModRight, yModRight);
 	context.stroke();
 	input.setAttribute("type", "text");
-	id.style.width = x2 - x1 + 'px';
+	id.style.width = second - first + 'px';
+	input.style.top = (addendMax - a) * 7 + 'px';
 }
+
+function randomInt(min, max) {
+    var rand = min + Math.random() * (max + 1 - min);
+    rand = Math.floor(rand);
+    return rand;
+  }
 
 left.onchange = function() {
 	if (left.value != a) {
@@ -46,12 +60,13 @@ left.onchange = function() {
 		left.style.border = "none";
 		left.style.backgroundColor = "white";
 		addendA.style.backgroundColor = "white";
-		draw(x2, x3, b, right, second);
+		draw(xFirst, xSecond, b, right, second);
 	}
 };
 left.onfocus = function(){
 	left.style.color = "black";
 };
+
 right.onchange = function() {
 	if (right.value != b) {
 		right.style.color = "red";
@@ -70,6 +85,7 @@ right.onchange = function() {
 right.onfocus = function(){
 	right.style.color = "black";
 };
+
 sum.onchange = function() {
 	if (sum.value != c) {
 		sum.style.color = "red";
